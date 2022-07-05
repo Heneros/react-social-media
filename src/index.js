42d9eb1e904021d42f58083aa098b75c1c241318
Heneros <punk60826@gmail.com>
2022-07-06 02:35:45 +0300

    fetch users

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import React, { useEffect } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-const endPoint = 'https://api.github.com/users/heneros';
+const baseUrl = 'https://api.github.com/users/';
 
 function App() {
-    const [user, setUser] = React.useState(null);
+    const [username, setUsername] = React.useState("Heneros");
 
+    const [user, setUser] = React.useState(null);
+    async function getUser(route) {
+        const response = await fetch(`${baseUrl}${username}`)
+        const data = await response.json()
+        setUser(data);
+    }
     React.useEffect(() => {
-        async function getUser() {
-            const response = await fetch(endPoint)
-            const data = await response.json()
-            setUser(data);
-        }
-        getUser()
+
+        getUser(username)
 
 
         // fetch(endPoint)
@@ -25,14 +27,23 @@ function App() {
 
     }, []);
 
-    return user ? (
-        <div>
-            <h1> {user.name} </h1>
-            <p> {user.bio} </p>
-            <p> {user.location} </p>
-            <img src={user.avatar_url} />
-        </div>
-    ) : <p>loading</p>;
+    return <div>
+        <input
+            type="text"
+            placeholder='Input username'
+            onChange={event => setUsername(event.target.value)}
+        />
+        <button onClick={getUser}>Search</button>
+        <button>Clear</button>
+        {user ? (
+            <div>
+                <h1> {user.name} </h1>
+                <p> {user.bio} </p>
+                <p> {user.location} </p>
+                <img src={user.avatar_url} />
+            </div>
+        ) : <p>loading</p>}
+    </div>
 }
 
 
