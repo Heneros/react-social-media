3a23e1a803c0386ead072e35ebe4487650b53e33
Heneros <punk60826@gmail.com>
2022-07-06 02:41:51 +0300

    function handleClearInput

--- src/index.js
+++ src/index.js
@@ -9,32 +9,30 @@ function App() {
     const [username, setUsername] = React.useState("Heneros");
 
     const [user, setUser] = React.useState(null);
+    const searchInput = React.useRef();
+
+    React.useEffect(() => {
+        getUser()
+    }, []);
+    function handleClearInput() {
+        searchInput.current.value = "";
+        searchInput.current.focus();
+    }
     async function getUser(route) {
         const response = await fetch(`${baseUrl}${username}`)
         const data = await response.json()
         setUser(data);
     }
-    React.useEffect(() => {
-
-        getUser(username)
-
-
-        // fetch(endPoint)
-        //     .then(response => response.json())
-        //     // .then(data => console.log(data))
-        //     .then(data => setUser(data));
-
-
-    }, []);
 
     return <div>
         <input
             type="text"
             placeholder='Input username'
             onChange={event => setUsername(event.target.value)}
+            ref={searchInput}
         />
         <button onClick={getUser}>Search</button>
-        <button>Clear</button>
+        <button onClick={handleClearInput}>Clear</button>
         {user ? (
             <div>
                 <h1> {user.name} </h1>
