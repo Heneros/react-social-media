b7447913b34833c12665e325a0d4b59427b5fcb2
Heneros <punk60826@gmail.com>
2022-07-21 02:42:15 +0300

    useReducer practice

--- src/App.js
+++ src/App.js
@@ -1,78 +1,57 @@
 import { useState } from "react";
 import { useReducer } from "react";
 import React from 'react';
-// import Login from './components/Login';
-// import Header from './components/Header';
-// import CreatePost from './components/CreatePost';
-// import PostList from './components/PostList';
-// import postReducer from './reducer';
+import Todo from "./Todo";
 
-// export const UserContext = React.createContext();
-// export const PostContext = React.createContext({
-//     posts: []
-// });
-
-const ACTIONS = {
-    INCREMENT: 'increment',
-    DECREMENT: 'decrement'
+export const ACTIONS = {
+    ADD_TODO: 'add-todo',
+    TOGGLE_TODO: 'toggle-todo',
+    DELETE_TODO: 'delete-todo'
 }
 
-function reducer(state, action) {
+function reducer(todos, action) {
     switch (action.type) {
-        case ACTIONS.INCREMENT:
-            return { count: state.count + 1 }
-        case ACTIONS.DECREMENT:
-            return { count: state.count - 1 }
+        case ACTIONS.ADD_TODO:
+            return [...todos, newTodo(action.payload.name)]
+        case ACTIONS.TOGGLE_TODO:
+            return todos.map(todo => {
+                if (todo.id === action.payload.id) {
+                    return { ...todo, complete: !todo.complete }
+                }
+                return todo;
+            })
+        case ACTIONS.DELETE_TODO:
+            return todos.filter(todo => todo.id !== action.payload.id)
         default:
-            return state;
+            return todos;
     }
 }
-function App() {
 
-    const [state, dispatch] = useReducer(reducer, { count: 0 });
-    const [count, setCount] = useState(0);
+function newTodo(name) {
+    return { id: Date.now(), name: name, complete: false }
+}
 
-    function increment() {
-        dispatch({ type: ACTIONS.INCREMENT })
+export default function App() {
+    const [todos, dispatch] = useReducer(reducer, []);
+    const [name, setName] = useState('');
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+        setName('');
     }
-    function decrement() {
-        dispatch({ type: ACTIONS.DECREMENT })
-    }
-    // const initialPostState = React.useContext(PostContext);
-    // const [state, dispatch] = React.useReducer(postReducer, initialPostState);
-    // const [user, setUser] = React.useState('Rustam');
-
-    // const [posts, setPosts] = React.useState([]);
-
-    // React.useEffect(() => {
-    //     document.title = user ? `${user}'s Feed` : "Please Login";
-    // }, [user]);
-
-    // // function handleAddPost(newPost) {
-    // //     setPosts([newPost, ...posts])
-    // // }
-
-    // if (!user) {
-    //     return <div> <Login user={user} setUser={setUser} /> </div>
-    // }
 
     return (
         <>
-            <button onClick={decrement}>-</button>
-            <span>{state.count} </span>
-            <button onClick={increment}>+</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            </form>
+            {todos.map(todo => {
+                return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+            })}
         </>
-        // <PostContext.Provider value={{ state, dispatch }}>
-        //     <UserContext.Provider value={user}>
-        //         <Header user={user} setUser={setUser} />
-        //         <CreatePost user={user}
-        //         // handleAddPost={handleAddPost} 
-        //         />
-        //         <PostList posts={state.posts} />
-        //     </UserContext.Provider>
-        // </PostContext.Provider>
-    );
+    )
 }
 
 
-export default App;
