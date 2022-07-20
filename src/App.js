b3820bf57f145ad005150eb19495c6ed6a703722
Heneros <punk60826@gmail.com>
2022-07-21 01:12:37 +0300

    practice

--- src/App.js
+++ src/App.js
@@ -12,19 +12,31 @@ import React from 'react';
 //     posts: []
 // });
 
-function reducer() {
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement'
+}
 
+function reducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.INCREMENT:
+            return { count: state.count + 1 }
+        case ACTIONS.DECREMENT:
+            return { count: state.count - 1 }
+        default:
+            return state;
+    }
 }
 function App() {
 
-    const [] = useReducer(reducer, { count: 0 });
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
     const [count, setCount] = useState(0);
 
     function increment() {
-        setCount(prevCount => prevCount + 1);
+        dispatch({ type: ACTIONS.INCREMENT })
     }
     function decrement() {
-        setCount(prevCount => prevCount + 1);
+        dispatch({ type: ACTIONS.DECREMENT })
     }
     // const initialPostState = React.useContext(PostContext);
     // const [state, dispatch] = React.useReducer(postReducer, initialPostState);
@@ -47,7 +59,7 @@ function App() {
     return (
         <>
             <button onClick={decrement}>-</button>
-            <span>{count} </span>
+            <span>{state.count} </span>
             <button onClick={increment}>+</button>
         </>
         // <PostContext.Provider value={{ state, dispatch }}>
