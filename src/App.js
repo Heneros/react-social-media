e48369c2bca37aa655c4fc5559689fb077e70c9b
Heneros <punk60826@gmail.com>
2022-08-01 01:03:43 +0300

    useContext and useReducer practice. To-Do app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useReducer } from "react";
 
-import FunctionContextComponent from "./FunctionContextComponent";
 
-import { ThemeProvider } from './ThemeContext';
+import Todo from "./Todo";
 
-export const ThemeContext = React.createContext();
+
+export const ACTIONS = {
+    ADD_BOX: 'add-box',
+    TOGGLE_BOX: 'toggle-box',
+    DELETE_BOX: 'delete-box'
+}
+function reducer(boxes, action) {
+    switch (action.type) {
+        case ACTIONS.ADD_BOX:
+            return [...boxes, newBox(action.payload.name)]
+        case ACTIONS.TOGGLE_BOX:
+            return boxes.map(box => {
+                if (box.id === action.payload.id) {
+                    return { ...box, complete: !box.complete }
+                }
+                return box;
+            })
+        case ACTIONS.DELETE_BOX:
+            return boxes.filter(box => box.id !== action.payload.id)
+            return boxes;
+        default:
+            return boxes;
+    }
+}
+function newBox(name) {
+    return { id: Date.now(), name: name, complete: false }
+}
 
 
 export default function App() {
-    // const [darkTheme, setDarkTheme] = useState(true);
 
-    // function toggleTheme() {
-    //     setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    // }
+    const [boxes, dispatch] = useReducer(reducer, []);
+    const [name, setName] = useState('');
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        dispatch({ type: ACTIONS.ADD_BOX, payload: { name: name } })
+        setName('');
+    }
 
     return (
         <>
-            <ThemeProvider>
-                <FunctionContextComponent />
-            </ThemeProvider>
+  
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={name} onChange={e => setName(e.target.value)} />
+
+            </form>
+            {boxes.map(box => {
+                return <Todo key={box.id} box={box} dispatch={dispatch} />
+            })}
+
         </>
 
     )
--- src/Todo.js
+++ src/Todo.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { ACTIONS } from './App';
 
 
-export default function Todo({ todo, dispatch }) {
+export default function Todo({ box, dispatch }) {
     return (
         <div>
-            <span style={{ color: todo.complete ? '#AAA' : '#000' }}>
-                {todo.name}
+            <span style={{ color: box.complete ? '#AAA' : '#000' }}>
+                {box.name}
             </span>
-            <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
-            <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
+            <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_BOX, payload: { id: box.id } })}>Toggle</button>
+            <button onClick={() => dispatch({ type: ACTIONS.DELETE_BOX, payload: { id: box.id } })}>Delete</button>
         </div>
     )
 }
