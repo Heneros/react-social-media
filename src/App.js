a9c8d7aa89f3bb9a43e9dde936e6c325d1f9bef0
Heneros <punk60826@gmail.com>
2022-08-01 01:09:08 +0300

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,62 +1,48 @@
-import React, { useState, createContext, useReducer } from "react";
+import React from 'react';
+import Login from './components/Login';
+import Header from './components/Header';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
+import postReducer from './reducer';
+
+export const UserContext = React.createContext();
+export const PostContext = React.createContext({
+    posts: []
+});
 
 
-import Todo from "./Todo";
+function App() {
 
+    const initialPostState = React.useContext(PostContext);
+    const [state, dispatch] = React.useReducer(postReducer, initialPostState);
+    const [user, setUser] = React.useState('Rustam');
 
-export const ACTIONS = {
-    ADD_BOX: 'add-box',
-    TOGGLE_BOX: 'toggle-box',
-    DELETE_BOX: 'delete-box'
-}
-function reducer(boxes, action) {
-    switch (action.type) {
-        case ACTIONS.ADD_BOX:
-            return [...boxes, newBox(action.payload.name)]
-        case ACTIONS.TOGGLE_BOX:
-            return boxes.map(box => {
-                if (box.id === action.payload.id) {
-                    return { ...box, complete: !box.complete }
-                }
-                return box;
-            })
-        case ACTIONS.DELETE_BOX:
-            return boxes.filter(box => box.id !== action.payload.id)
-            return boxes;
-        default:
-            return boxes;
-    }
-}
-function newBox(name) {
-    return { id: Date.now(), name: name, complete: false }
-}
+    const [posts, setPosts] = React.useState([]);
 
+    React.useEffect(() => {
+        document.title = user ? `${user}'s Feed` : "Please Login";
+    }, [user]);
 
-export default function App() {
+    // function handleAddPost(newPost) {
+    //     setPosts([newPost, ...posts])
+    // }
 
-    const [boxes, dispatch] = useReducer(reducer, []);
-    const [name, setName] = useState('');
-
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        dispatch({ type: ACTIONS.ADD_BOX, payload: { name: name } })
-        setName('');
+    if (!user) {
+        return <div> <Login user={user} setUser={setUser} /> </div>
     }
 
     return (
-        <>
-  
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={name} onChange={e => setName(e.target.value)} />
-
-            </form>
-            {boxes.map(box => {
-                return <Todo key={box.id} box={box} dispatch={dispatch} />
-            })}
-
-        </>
-
-    )
+        <PostContext.Provider value={{ state, dispatch }}>
+            <UserContext.Provider value={user}>
+                <Header user={user} setUser={setUser} />
+                <CreatePost user={user}
+                // handleAddPost={handleAddPost} 
+                />
+                <PostList posts={state.posts} />
+            </UserContext.Provider>
+        </PostContext.Provider>
+    );
 }
 
+
+export default App;
